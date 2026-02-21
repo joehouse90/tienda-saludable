94092ec962c7f3aab965fd9b9dc309eca8606bc3
joehouse90 <jsimoesdaniel@gmail.com>
2026-02-20 22:16:24 -0300

    fix: remove github pages base for vercel

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/tienda-saludable/', 
-  plugins: [react()]
-})
-
-
+  plugins: [react()],
+});
